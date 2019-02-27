e353bffb43f9812c1cfa030c33048a49a2e16c53
baranork <baranork00@gmail.com>
2019-02-27 12:26:02 -0300

    Add files via upload

--- 6-InstruccionFor/jsIteracionesFor (3).js
+++ 6-InstruccionFor/jsIteracionesFor (3).js
@@ -1,8 +1,15 @@
 function Mostrar()
 {
 
-var repetciones = prompt("ingrese el número de repeticiones");
+var repeticiones = parseInt(prompt("ingrese el número de repeticiones"));
 
+while(isNaN(repeticiones)){
+  repeticiones = parseInt(prompt("ingrese el número de repeticiones"));
+}
 
+for(var conteo = 0; repeticiones > conteo ; conteo++){
+  console.log("Hola UTNFRA");
+
+}
 
 }//FIN DE LA FUNCIÓN
